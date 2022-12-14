ccf3ebb48c2b4b29e51db1ede6819da87051e559
SamSvetlik <sam.svetlik@gmail.com>
2022-12-14 17:06:55 -0600

    chore: create table with original listings

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -1,13 +1,38 @@
 import React from 'react'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const Listings = (props) => {
     return (
-        <div >
-            <h1>
-            This is where the listings will go.
-            </h1>
-            {console.log(props.businesses)}
-        </div>
+        <TableContainer component={Paper} elevation={8} >
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Business Name</TableCell>
+                        <TableCell>Address</TableCell>
+                        <TableCell>Hours</TableCell>
+                        <TableCell>More Info</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {props.businesses.map((business)=> {
+                        return (
+                            <TableRow>
+                                <TableCell>{business.name}</TableCell>
+                                <TableCell>{business.address}</TableCell>
+                                <TableCell>{business.hours.reduce((prev, cur) => [ ...prev, cur, <br /> ], [])}</TableCell>
+                                <TableCell>Click for more</TableCell>
+                            </TableRow>
+                        )
+                    })}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
 
