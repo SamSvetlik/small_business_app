e918f036079bf86bec118941d91c5ad9e1cd1792
SamSvetlik <sam.svetlik@gmail.com>
2022-12-14 21:05:49 -0600

    attempted to add map functionality. got a map, but no functionality. businesses have position property.

--- src/redux/state.js
+++ src/redux/state.js
@@ -18,8 +18,9 @@ export default {
             address: "1406 Avenue Q, Lubbock, Tx 79401",
             hours: ["Monday-Saturday: 11 am - midnight"],
             url: "https://www.flipperstavern.com/",
-            lat: 33.582340,
-            lng: -101.855620
+            position: {
+                lat: 33.582340,
+                lng: -101.855620}
         },
         {
             name: "J&B Coffee Co",
@@ -27,8 +28,9 @@ export default {
             address: "2701 26th St, Lubbock, Tx 79410",
             hours: ["Monday-Saturday: 6:30 am - 9 pm", "Sunday 8 am - 9 pm"],
             url: "https://jandbcoffee.com/",
-            lat: 33.570300,
-            lng: -101.875590
+            position: {
+                lat: 33.570300,
+                lng: -101.875590}
         },
         {
             name: "Star Comics",
@@ -36,8 +38,9 @@ export default {
             address: "3504 34th St, Lubbock, Tx 79410",
             hours: ["Monday-Saturday: 11 am - 6 pm", "Sunday: 1 pm - 5 pm"],
             url: "https://www.starcomicsonline.com/",
-            lat: 33.563610,
-            lng: -101.890360
+            position: {
+                lat: 33.563610,
+                lng: -101.890360}
         },
         {
             name: "Ralph's Records",
@@ -45,8 +48,9 @@ export default {
             address: "3322 82nd St, Lubbock, Tx 79423",
             hours: ["Monday-Wednesday: 9 am - 9 pm", "Thursday: 9 am - 8 pm", "Friday-Saturday: 9 am - 9 pm", "Sunday: 11 am - 8 pm"],
             url: "https://ralphsrecordstx.com/",
-            lat: 33.520190,
-            lng: -101.886700
+            position: {
+                lat: 33.520190,
+                lng: -101.886700}
         }
     ]
 
